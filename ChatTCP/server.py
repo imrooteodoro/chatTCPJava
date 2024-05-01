3f4acf106924eb8c8c4d503bb4e106b5237e2035
RangersonTI <rangerson14@outlook.com>
2024-05-01 11:37:05 -0300

    Update
    * Implementação da Interface Gráfica do servidor em TkInter e ligação dos campo com o backend

---
+++ ChatTCP/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+import sys
+
+
+app =  Flask(__name__)
+app.config['SECRET'] = "secret!123"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('message')
+
+def handle_message(message):
+    print("Received message: " + message)
+    if message != "User connected!":
+        send(message, broadcast=True)
+
+@app.route('/')
+
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    porta_server = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    socketio.run(app, host="localhost", port= porta_server)
