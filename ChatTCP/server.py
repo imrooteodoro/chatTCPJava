1ea7e0075638da5df58940838c89ffc1c7d63838
RangersonTI <rangerson14@outlook.com>
2024-05-04 08:20:56 -0300

    Haviso previo no Chat quando um novo usuário se conectar

--- ChatTCP/server.py
+++ ChatTCP/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
 import sys
 
@@ -6,17 +6,40 @@ app =  Flask(__name__)
 app.config['SECRET'] = "secret!123"
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on('message')
+# Usuarios Conectados
+usuarios_conectados = []
 
-def handle_message(message):
-    print("Messagem recebida: " + message)
-    if message != "User connected!":
-        send(message, broadcast=True)
 
 @app.route('/')
 
 def index():
-    return render_template("login.html")
+    return render_template("index.html")
+
+@socketio.on('message')
+def handle_message(message):
+    print("Messagem recebida: " + message)
+    if message != "User connected!":
+       # socketio.emit('message', message, broadcast=True)
+        send(message,broadcast=True)
+
+@socketio.on('connect')
+def handle_connect():
+    # Envia uma mensagem de aviso para todos os clientes que um novo usuário se conectou
+    #for cliente in usuarios_conectados:
+        #socketio.emit('user_connected', {'user_count': len(usuarios_conectados)}, room=cliente)
+    usuarios_conectados.append(request.sid)
+    print("\n Conectados: "+str(usuarios_conectados)+"\n")
+    if request.sid in usuarios_conectados:
+        send('1234 conectou', broadcast=True)
+        print('um cliente se conectou')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    usuarios_conectados.remove(request.sid)
+    print("\n Conectados: "+str(usuarios_conectados)+"\n")
+    if not request.sid in usuarios_conectados:
+        send('1234 desconectou', broadcast=True)
+        print("um cliente se desconectou")
 
 if __name__ == "__main__":
     porta_server = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
